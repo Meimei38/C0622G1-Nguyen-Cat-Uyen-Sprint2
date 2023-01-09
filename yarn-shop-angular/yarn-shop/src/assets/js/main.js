346945f4da4635955736992ea6ebf520e252091b
Meimei38 <uyennguyencat@gmail.com>
2023-01-09 18:24:56 +0700

    update demo jwt

--- yarn-shop-angular/yarn-shop/src/assets/js/main.js
+++ yarn-shop-angular/yarn-shop/src/assets/js/main.js
@@ -146,7 +146,7 @@
 
     // Uncomment below and use your date //
 
-    var timerdate = "2023/01/07"
+    var timerdate = "2023/01/08"
 
     $("#countdown").countdown(timerdate, function (event) {
       $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
